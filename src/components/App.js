65ee9aac37cac6ca4fb9e1348f386153b53df097
tvpeter <withtvpeter@gmail.com>
2019-09-17 17:25:01 +0100

    ch(setup project): configure webpack and an empty react project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../assets/App.css";
+
+class App extends Component {
+    render () {
+        return (
+            <div className="App">
+                <h4> Hello  </h4>
+                <p>from Currency Converter</p>
+            </div>
+        );
+    }
+}
+
+export default App;
