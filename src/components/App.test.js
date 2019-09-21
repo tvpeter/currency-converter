35913dfa4d77fa64db7721257a6b721e54c652c2
tvpeter <withtvpeter@gmail.com>
2019-09-21 03:08:15 +0100

    chore(travis integration): Add travis script

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 describe('First React component test with Enzyme', () => {
-    it('renders without crashing', () => {
-        shallow(<App />);
-    });
+  it('renders without crashing', () => {
+      shallow(<App />);
+  });
 });
